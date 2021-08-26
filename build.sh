9289d96f1e97dc24ba8c25bdcdccc9b074aa8e5b
Erwan Egasse <erwan.egasse@gmail.com>
2021-08-26 10:10:10 +0200

    fix baseurl

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ echo 'Starting build... Cleaning up old content.'
 rm -rf public docs
 hugo
 mv public docs
+echo 'Adding CNAME...'
+touch ./docs/CNAME
+echo 'rebase.erwan.sh' > ./docs/CNAME
 echo 'Build done.'
